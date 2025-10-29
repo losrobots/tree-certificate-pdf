489fd24effb3e9c4d939459182796d1c6cc5c444
losrobots <phil@losrobots.io>
2025-10-29 12:17:07 -0400

    update title

--- src/templates/tree-certificate-template.ts
+++ src/templates/tree-certificate-template.ts
@@ -6,7 +6,7 @@ const html: string = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>PDF</title>
+    <title>Memorial Tree Certificate - {{recipientHeading}} {{recipientName}}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
